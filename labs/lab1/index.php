e71951a6fdcf32440e7447286d746f00b8a1fb72
dmitriytheprogrammer <dmitriy.only.work@mail.ru>
2023-02-21 10:17:17 +0300

    changes

---
+++ labs/lab1/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../lab1/index.php">
+    <link rel="stylesheet" href="../lab1/style.css">
+    <title>Lab.work №1 Зернов Дмитрий Михайлович 221-323</title>
+</head>
+
+<body>
+    <header class="header-wrapper">
+        <img width="200px" class="img" src="../lab1/logo.png" alt="polytechlogo">
+        <h1 class="header__task">Тема 1. Домашняя работа: Hello, World!</h1>
+        <h2 class="header__full-name">Dmitriy Zernov 221-323</h2>
+    </header>
+
+
+    <main class="container">
+    <p class="container__task"><?php echo "Hello, World!";?></p>
+    </main>
+
+
+    <footer class="container2">
+        <p class="container2__task-desc">Создать веб-страницу с динамическим контентом. 
+            Загрузить код в удаленный репозиторий. Залить на хостинг.</p>
+    </footer>
+
+
+</body>
+
+</html>
